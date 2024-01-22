85672dbae9ddfed01e19bf842e784f71b740e2a7
Kseniia Telegina <ksusha2993@gmail.com>
2024-01-22 09:13:46 +0200

    Прописан JS для создания и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function createCard(data, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+  
+    deleteButton.addEventListener('click', function() {
+      deleteCallback(cardElement);
+    });
+  
+    return cardElement;
+  }
+  
+  function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+  
+  const placesList = document.querySelector('.places__list');
+  initialCards.forEach(function(cardData) {
+    const newCard = createCard(cardData, deleteCard);
+    placesList.appendChild(newCard);
+  });
